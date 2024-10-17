87f7f0de438166e9888a1cd2a848c104080aa2c1
JOAN_CORTES_TORANDELL <joancortes@paucasesnovescifp.cat>
2024-10-17 18:17:54 +0200

    Apuntes DOM 17/10/2024

---
+++ Apuntes/Tema 2 DOM/DOM.js
@@ -0,0 +1,21 @@
+var v1 = document.getElementById("mujer");
+console.log(V1);
+
+var v2 = document.getElementsByTagName("input");
+console.log(v2);
+
+var v3 = document.getElementsByTagName("body");
+console.log(v3);
+console.log(v3[0]); /* trabajar con el array de body */
+console.log(v3[0].childNodes);
+console.log(v3[0].children);
+
+
+var v4 = document.getElementsByClassName("sexo");
+console.log(v4);
+
+var v5 = document.getElementById("sexo");
+console.log(v5);
+
+var v6 = document.getRootNode();
+console.log(v6);
