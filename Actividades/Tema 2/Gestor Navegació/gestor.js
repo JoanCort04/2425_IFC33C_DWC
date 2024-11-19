90038a7b0fe6e840748dd2eff893e646d9e8102d
JOAN_CORTES_TORANDELL <joancortes@paucasesnovescifp.cat>
2024-11-19 16:52:51 +0100

    Penjat el gestor de navegació web complet

---
+++ Actividades/Tema 2/Gestor Navegació/gestor.js
@@ -0,0 +1,51 @@
+/* Visualización URL Actual */
+
+function mostrarURLActual() {
+  alert(window.location);
+}
+
+function irAtras() {
+  if (window.history.length > 1) {
+    window.history.back();
+  } else {
+    confirm("no");
+  }
+}
+
+function irAdelante() {
+  if (window.history.length > 1) {
+    window.history.forward();
+  } else {
+    confirm("no");
+  }
+}
+
+function mostrarInfoNavegador() {
+  let message =
+    "Utilitzant: " +
+    window.navigator.userAgent +
+    "\nAmb llenguatge: " +
+    window.navigator.language +
+    "\nEstat de connexió: " +
+    window.navigator.onLine;
+  confirm(message);
+}
+
+function redirigirURL() {
+    url = document.getElementById("urlInput").value;
+    window.location.assign(url);
+}
+
+var pagina;
+
+function abrirPopup() {
+  pagina = window.open(
+    "https://paucasesnovescifp.cat",
+    "popup",
+    "width=600,height=400"
+  );
+}
+
+function cerrarPopup() {
+    pagina.window.close();
+}
